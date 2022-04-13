01420ed8a28f8c4d34762184d62c62c84471fa34
IscoRuta98 <fmruta98@gmail.com>
2022-04-13 21:01:02 +0200

    feat: add_directory
    The detect.py file takes a directory (of where the PDFs are stored) as
    input, and output the number of: tables, equations, and figures an
    article has.

--- Detect.py
+++ Detect.py
@@ -4,6 +4,11 @@ import layoutparser as lp
 import matplotlib.pyplot as plt
 from PyPDF2 import PdfFileReader, PdfFileWriter
 from pdf2image import convert_from_path
+import os
+
+# directory path
+dirPath = r'/home/isco/Documents/SamplePapers/'
+ListOfPapers = os.listdir(dirPath)
 
 # Load the deep layout models from the layoutparser API
 model = lp.Detectron2LayoutModel('lp://PubLayNet/mask_rcnn_X_101_32x8d_FPN_3x/config',
@@ -15,41 +20,45 @@ formula_model = lp.Detectron2LayoutModel(
     label_map={1: "Equation"})
 
 # Read in the PDF
+for paper in range(len(ListOfPapers)):
 
-pdf_document = "559.pdf"
-pdf = PdfFileReader(pdf_document)
-images = convert_from_path(pdf_document)
+    pdf_document = dirPath + ListOfPapers[paper]
+    print("Checking Article: ", ListOfPapers[paper])
+    pdf = PdfFileReader(pdf_document)
+    images = convert_from_path(pdf_document)
 
-# Set up a counter to keep track.
-NumOfTables = 0
-NumOfFigures = 0
-NumOfEquation = 0
+    # Set up a counter to keep track.
+    NumOfTables = 0
+    NumOfFigures = 0
+    NumOfEquation = 0
 
-for i in range(pdf.getNumPages()):
-    """
-    For every page in an article, two Deep Learning models are ran.
-    One DL model detects: figures, and tables.
-    The other model detects equations in a page.
-    
-    Once the above is detected, there is a counter that keeps track of:
-    - The number of tables.
-    - THe number of equations.
-    - The number of figures.
-    """
-    layout = model.detect(images[i])
-    EquationLayout = formula_model.detect(images[i])
-    # Export layout results as a dataframe.
-    df = layout.to_dataframe()
-    df2 = EquationLayout.to_dataframe()
-    # Extract the number of layouts (i.e. rows in the dataframe) that detected a table.
-    Table = df[df['type'].str.contains('Table')]
-    Figure = df[df['type'].str.contains('Figure')]
-    # Equation = df2[df2['type'] == 'Equation']
+    for i in range(pdf.getNumPages()):
+        """
+        For every page in an article, two Deep Learning models are ran.
+        One DL model detects: figures, and tables.
+        The other model detects equations in a page.
+        
+        Once the above is detected, there is a counter that keeps track of:
+        - The number of tables.
+        - THe number of equations.
+        - The number of figures.
+        """
+        layout = model.detect(images[i])
+        EquationLayout = formula_model.detect(images[i])
+        # Export layout results as a dataframe.
+        df = layout.to_dataframe()
+        df2 = EquationLayout.to_dataframe()
+        # Extract the number of layouts (i.e. rows in the dataframe) that detected a table.
+        Table = df[df['type'].str.contains('Table')]
+        Figure = df[df['type'].str.contains('Figure')]
+        # Equation = df2[df2['type'] == 'Equation']
 
-    NumOfTables = NumOfTables + len(Table)
-    NumOfFigures = NumOfFigures + len(Figure)
-    NumOfEquation = NumOfEquation + len(df2)
+        NumOfTables = NumOfTables + len(Table)
+        NumOfFigures = NumOfFigures + len(Figure)
+        NumOfEquation = NumOfEquation + len(df2)
 
-print("The numer of Tables in this image is: ", len(NumOfTables))
-print("The numer of Figures in this image is: ", len(NumOfFigures))
-print("The numer of Equations in this image is: ", len(NumOfEquation))
+    print("The numer of Tables in this article is: ", NumOfTables)
+    print("The numer of Figures in this article is: ", NumOfFigures)
+    print("The numer of Equations in this article is: ", NumOfEquation)
+
+    print("Next article...")
